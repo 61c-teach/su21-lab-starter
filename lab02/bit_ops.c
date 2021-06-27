a893a2e5f78efe7a3e32a2c3da589104ee87167c
Class Account <cs61c-tae@imail.EECS.Berkeley.EDU>
2021-06-27 10:38:23 -0700

    [Lab02] Updating starter code

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -8,12 +8,12 @@ unsigned get_bit(unsigned x, unsigned n) {
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
-void set_bit(unsigned * x, unsigned n, unsigned v) {
+void set_bit(unsigned *x, unsigned n, unsigned v) {
     /* YOUR CODE HERE */
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
-void flip_bit(unsigned * x, unsigned n) {
+void flip_bit(unsigned *x, unsigned n) {
     /* YOUR CODE HERE */
 }
 
